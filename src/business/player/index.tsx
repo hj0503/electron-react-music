6c7aec822db15f64eb844a158cdc2f674d9bbfe7
hj0503 <857131753@qq.com>
2021-09-02 21:35:27 +0800

    feat: 添加播放器样式

--- src/business/player/index.tsx
+++ src/business/player/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import Icon from '@ant-design/icons';
 import styles from './index.module.scss';
+import { PlayNextSvg, PlayPrevSvg, PlayRandomSvg, VoiceSvg } from '@/components/svg';
 
 const Player: React.FC = () => {
   return (
     <div className={styles.player}>
-      <div className={styles.player__way}>播放方式</div>
-      <div className={styles.player__play}>
-        <div className={styles['player__play-prev']}>上一首</div>
-        <div className={styles['player__play-status']}>播放中</div>
-        <div className={styles['player__play-next']}>下一首</div>
+      <div className={styles.player__way}>
+        <Icon component={PlayRandomSvg} />
       </div>
-      <div className={styles.player__voice}>音量</div>
+      <div className={styles.player__play}>
+        <div className={styles['player__play-prev']}>
+          <Icon component={PlayPrevSvg}></Icon>
+        </div>
+        <div className={styles['player__play-status']}>播放中</div>
+        <div className={styles['player__play-next']}>
+          <Icon component={PlayNextSvg}></Icon>
+        </div>
+      </div>
+      <div className={styles.player__voice}>
+        <Icon component={VoiceSvg}></Icon>
+      </div>
     </div>
   );
 };
