85686a9b23640bdec2b2eaa40489c0968199f763
hj0503 <857131753@qq.com>
2022-05-06 18:01:32 +0800

    feat: menu-tabs样式修改

--- src/components/menu-tabs/index.tsx
+++ src/components/menu-tabs/index.tsx
@@ -1,7 +1,8 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import styles from './menu-tabs.module.less';
 import { MenuType } from '@/config/side-menus';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
 
 interface Props {
   tabs: Array<MenuType>;
@@ -9,16 +10,31 @@ interface Props {
 const MenuTabs: FC<Props> = props => {
   const { tabs } = props;
   const { push } = useHistory();
+  const { pathname } = useLocation();
+  const activeBarEl = useRef<HTMLDivElement>(null);
   const clickTabBar = (path: string) => {
     push(path);
   };
+
+  const [inkTabStyles, setInkTabStyles] = useState({});
+  useEffect(() => {
+    const style = {}
+    if (activeBarEl.current) {
+      setInkTabStyles(activeBarEl.current.offsetWidth);
+    }
+  }, []);
+
   return (
     <div className={styles['menu-tabs']}>
+      <span style={inkTabStyles} className={styles['menu-tabs__ink-tab']}></span>
       {tabs &&
         tabs.map(tab => (
           <span
-            className={styles['menu-tabs__tab']}
+            className={classNames(styles['menu-tabs__tab'], {
+              [styles['is-active']]: tab.path === pathname,
+            })}
             key={tab.label}
+            ref={tab.path === pathname ? activeBarEl : null}
             onClick={() => clickTabBar(tab.path)}
           >
             {tab.label}
