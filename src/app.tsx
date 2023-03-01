c9749d84b028634f6ecf8625a1d3e1f8a25a6b7e
hj0503 <857131753@qq.com>
2023-03-01 22:00:24 +0800

    测试git-pages

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,5 @@
-import { Spin } from 'antd';
-import { FC, Suspense } from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { FC } from 'react';
 
-import Header from '@/layout/header';
-import routes, { Page } from '@/router';
-import store from '@/store';
-import { classGenerator } from '@/utils';
-
-const getClass = classGenerator('main');
-
-const App: FC = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Header />
-      <Suspense
-        fallback={<Spin className={getClass('loading')} style={{ position: 'absolute' }} />}
-      >
-        <Switch>
-          <Redirect from={Page.root} to={Page.findMusic} exact />
-          {routes.map(route => (
-            <Route key={route.path} path={route.path}>
-              <route.component {...route} />
-            </Route>
-          ))}
-        </Switch>
-      </Suspense>
-    </BrowserRouter>
-  </Provider>
-);
+const App: FC = () => <div>111</div>;
 
 export default App;
