723f58d60e9ad8921fb346fc5b4f36a94cd8dfb8
hj0503 <857131753@qq.com>
2023-03-08 21:57:43 +0800

    添加enum文件

---
+++ wyy-music/src/enum/index.ts
@@ -0,0 +1,17 @@
+export enum PlayMode {
+  IN_TURN,
+  LOOP,
+  SOLO,
+  RANDOM,
+}
+
+export enum SearchType {
+  SONG = 1,
+  SINGER = 100,
+  ALBUM = 10,
+  VIDEO = 1014,
+  PLAYLIST = 1000,
+  LYRIC = 1006,
+  RADIO = 1009,
+  USER = 1002,
+}
