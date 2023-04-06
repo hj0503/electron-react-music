7b59a01de0b20a5aba11e9c59f81562a15c725b8
hj0503 <857131753@qq.com>
2023-04-06 22:42:43 +0800

    初始化

--- wyy-music-vue3/src/App.vue
+++ wyy-music-vue3/src/App.vue
@@ -1,33 +1,7 @@
 <template>
-<el-button @click="test">222222</el-button>
-  <div class="test">
-
-    <el-popover
-      :visible="visible"
-      trigger="click"
-      title="With title"
-      :show-arrow="false"
-      :teleported="false"
-    >
-      <span> Some content </span>
-    </el-popover>
-  </div>
+  <div>初始化</div>
 </template>
 
-<script setup lang="ts">
-import { ref, unref } from 'vue';
-import { ClickOutside as vClickOutside } from 'element-plus';
-const visible = ref(true);
-const test = () => {
-  visible.value = !visible.value
-}
-</script>
+<script setup lang="ts"></script>
 
-<style>
-.test {
-  position: relative;
-  margin-top: 100px;
-  height: 2000px;
-  border: 1px solid #000000;
-}
-</style>
+<style></style>
